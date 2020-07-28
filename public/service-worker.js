59e9e1a381b34888022ea3bfedffb34e6c93e46b
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-07-28 17:43:14 -0500

    fix errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,10 @@
 const APP_PREFIX = 'BudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+
 const FILES_TO_CACHE = [
     './index.html',
-    './css/style.css',
+    './css/styles.css',
     './js/index.js',
     './js/idb.js',
     './manifest.json',
@@ -16,6 +17,7 @@ const FILES_TO_CACHE = [
     './icons/icon-96x96.png',
     './icons/icon-72x72.png'
 ];
+
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -23,41 +25,38 @@ self.addEventListener('install', function (e) {
             return cache.addAll(FILES_TO_CACHE)
         })
     )
-})
-self.addEventListener('activate', function(e) {
+});
+
+self.addEventListener('activate', function (e) {
     e.waitUntil(
-        caches.keys().then(function(keyList) {
-            let cacheKeeplist = keyList.filter(function(key) {
+        caches.keys().then(function (keyList) {
+        let cacheKeeplist = keyList.filter(function (key) {
             return key.indexOf(APP_PREFIX);
-            });
+        });
             cacheKeeplist.push(CACHE_NAME);
-    
-            return Promise.all(
-            keyList.map(function(key, i) {
+
+            return Promise.all(keyList.map(function (key, i) {
                 if (cacheKeeplist.indexOf(key) === -1) {
-                console.log('deleting cache : ' + keyList[i]);
+                console.log('deleting cache : ' + keyList[i] );
                 return caches.delete(keyList[i]);
                 }
-            })
-            );
+            }));
         })
-    );
+    )
 });
 
 self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) { // if cache is available, respond with cache
-            console.log('responding with cache : ' + e.request.url)
-            return request
-            } else {       // if there are no cache, try fetching request
-            console.log('file is not cached, fetching : ' + e.request.url)
-            return fetch(e.request)
-            }
-    
-            // You can omit if/else for console.log & put one line below like this too.
-            // return request || fetch(e.request)
-        })
-    )
-})
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+    })
+  )
+});
